178d21df0d9ea9965ea4009310b4864120c3e16a
Christian Pieczewski <christian@pieczewski.de>
2020-12-05 22:45:45 +0100

    removel old stuff

--- extensions/panache/elasticsearch-panache/deployment/src/main/java/io/quarkus/elasticsearch/panache/deployment/PanacheElasticsearchEntityEnhancer.java
+++ extensions/panache/elasticsearch-panache/deployment/src/main/java/io/quarkus/elasticsearch/panache/deployment/PanacheElasticsearchEntityEnhancer.java
@@ -6,12 +6,9 @@ import java.util.List;
 import java.util.Map;
 
 import org.jboss.jandex.ClassInfo;
-import org.jboss.jandex.DotName;
 import org.jboss.jandex.FieldInfo;
 import org.jboss.jandex.IndexView;
 import org.objectweb.asm.ClassVisitor;
-import org.objectweb.asm.MethodVisitor;
-import org.objectweb.asm.Opcodes;
 
 import io.quarkus.elasticsearch.panache.runtime.ElasticsearchOperations;
 import io.quarkus.gizmo.DescriptorUtils;
@@ -30,48 +27,14 @@ public class PanacheElasticsearchEntityEnhancer
     final Map<String, EntityModel> entities = new HashMap<>();
 
     public PanacheElasticsearchEntityEnhancer(IndexView index, List<PanacheMethodCustomizer> methodCustomizers) {
-        super(index, ElasticsearchProcessor.DOTNAME_PANACHE_ENTITY_BASE, methodCustomizers);
+        super(index, methodCustomizers);
         modelInfo = new MetamodelInfo<>();
     }
 
     @Override
     public ClassVisitor apply(String className, ClassVisitor outputClassVisitor) {
-        return new PanacheElasticsearchEntityClassVisitor(className, outputClassVisitor, modelInfo, panacheEntityBaseClassInfo,
-                indexView.getClassByName(DotName.createSimple(className)), methodCustomizers);
-    }
-
-    static class PanacheElasticsearchEntityClassVisitor extends PanacheEntityClassVisitor<EntityField> {
-
-        public PanacheElasticsearchEntityClassVisitor(String className, ClassVisitor outputClassVisitor,
-                MetamodelInfo<EntityModel<EntityField>> modelInfo, ClassInfo panacheEntityBaseClassInfo,
-                ClassInfo entityInfo, List<PanacheMethodCustomizer> methodCustomizers) {
-            super(className, outputClassVisitor, modelInfo, panacheEntityBaseClassInfo, entityInfo, methodCustomizers);
-        }
-
-        @Override
-        protected void injectModel(MethodVisitor mv) {
-            mv.visitLdcInsn(thisClass);
-        }
-
-        @Override
-        protected String getModelDescriptor() {
-            return "Ljava/lang/Class;";
-        }
-
-        @Override
-        protected String getPanacheOperationsBinaryName() {
-            return ELASTICSEARCH_OPERATIONS_BINARY_NAME;
-        }
-
-        @Override
-        protected void generateAccessorSetField(MethodVisitor mv, EntityField field) {
-            mv.visitFieldInsn(Opcodes.PUTFIELD, thisClass.getInternalName(), field.name, field.descriptor);
-        }
-
-        @Override
-        protected void generateAccessorGetField(MethodVisitor mv, EntityField field) {
-            mv.visitFieldInsn(Opcodes.GETFIELD, thisClass.getInternalName(), field.name, field.descriptor);
-        }
+        // TODO Auto-generated method stub
+        return null;
     }
 
     @Override
